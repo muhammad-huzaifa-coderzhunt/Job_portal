e63d5aeb81c6ed95defb3ba1428275943a81a4ca
Muhammad Huzaifa <muhammad.huzaifa@coderzhunt.com>
2025-08-28 13:11:12 -0700

    refactor(backend): use conventional Authorization HTTP header
    - replace custom HTTP header, 'x-auth-token' with 'Authorization' header as it is the conventional approach
    - Spliting the value of Authorization header and extracting the token part to token variable

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
   // Get token from header
-  const token = req.header('x-auth-token');
+const token = req.header('Authorization')?.split(' ')[1] || null;
 
   // Check if not token
   if (!token) {
